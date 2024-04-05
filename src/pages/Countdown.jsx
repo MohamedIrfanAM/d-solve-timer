d3ab2e8eb7e49123027d10cc2542a8a6e2395471
Mohamed Irfan <i4irfan777@gmail.com>
2024-04-05 16:48:58 +0530

    refreshment break

--- src/pages/Countdown.jsx
+++ src/pages/Countdown.jsx
@@ -19,7 +19,7 @@ function Countdown() {
   }, []);
 
   function calculateTimeLeft() {
-    const difference = +new Date("2024-04-05T17:00:00") - +new Date();
+    const difference = +new Date("2024-04-05T17:15:00") - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
